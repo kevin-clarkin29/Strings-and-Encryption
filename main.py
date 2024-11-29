627defa8803a3fde0dea39e483657a412127e529
BasedToph <158231610+BasedToph@users.noreply.github.com>
2024-11-29 11:48:39 -1000

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-'''from graph import Graph
+'''
+from graph import Graph
 from node import Node
 from message import Message
 from algorithms.rle import run_length_encode
-from algorithms.rsa import generate_keys, rsa_encrypt, rsa_decrypt'''
+from algorithms.rsa import generate_keys, rsa_encrypt, rsa_decrypt
+'''
